d017da64cc05272be6a1310441378195b81cc089
sarmakumaran2-hub <sarmakumaran2@gmail.com>
2026-02-09 09:07:49 +0530

    Add files via upload
    detail 02

---
+++ detail2.py
@@ -0,0 +1,10 @@
+firstname="Yogan"
+lastname="Seelan"
+email="yogaseelan@gmail.com"
+salary="Rs.50000"
+
+
+print(" \t    *******\n   \tEMPLOYEE DETAILS \n\t    *******")
+print("full name       :",firstname,lastname)
+print("email           :",email)
+print("basic salary is :",salary)
